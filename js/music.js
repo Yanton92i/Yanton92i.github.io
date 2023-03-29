bbe7ce5f28beea2acfdf0b31e4f0c2bce45657ea
Yanton92i <yaniss.benyagoub@gmail.com>
2023-03-28 20:00:57 -0800

    change

--- js/music.js
+++ js/music.js
@@ -1,11 +1,24 @@
+const audioFiles = [
+  'music/music1.mp3',
+  'music/music2.mp3',
+  'music/music3.mp3',
+  'music/music4.mp3',
+  'music/music5.mp3'
+];
+
 const audio = new Audio();
-audio.src = 'music/music1.mp3';
 audio.loop = true;
 audio.volume = 0.5;
 
+function playRandomTrack() {
+  const randomIndex = Math.floor(Math.random() * audioFiles.length);
+  audio.src = audioFiles[randomIndex];
+  audio.play();
+}
+
 function toggleMusic() {
   if (audio.paused) {
-    audio.play();
+    playRandomTrack();
   } else {
     audio.pause();
   }
@@ -13,3 +26,4 @@ function toggleMusic() {
 
 const button = document.querySelector('.connect');
 button.addEventListener('click', toggleMusic);
+
