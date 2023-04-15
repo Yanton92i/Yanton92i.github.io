4931b8e29e53e4a5cc4e801762478df073d55508
Yanton92i <yaniss.benyagoub@gmail.com>
2023-04-15 09:29:51 -0800

    modal

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function toggleExpand(element, className) {
+  element.classList.toggle(className);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Send Coin
+  const sendArrow = document.querySelector('.swap_right .coin_selector_arrow');
+  const sendArrowMobile = document.querySelector('.swap_right .coin_selector_arrow_mobile');
+  const sendExpand = document.querySelector('.send_coin_expand');
+
+  // Receive Coin
+  const receiveArrow = document.querySelector('.swap_receive_right .coin_selector_arrow');
+  const receiveArrowMobile = document.querySelector('.swap_receive_right .coin_selector_arrow_mobile');
+  const receiveExpand = document.querySelector('.receive_coin_expand');
+
+  // Event listeners for desktop
+  sendArrow.addEventListener('click', () => toggleExpand(sendExpand, 'show'));
+  receiveArrow.addEventListener('click', () => toggleExpand(receiveExpand, 'show'));
+
+  // Event listeners for mobile
+  sendArrowMobile.addEventListener('click', () => toggleExpand(sendExpand, 'show'));
+  receiveArrowMobile.addEventListener('click', () => toggleExpand(receiveExpand, 'show'));
+});
