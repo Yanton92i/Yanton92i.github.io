c0a69a0b54fb8ec42f935df52cdec69314499b9b
Yanton92i <yaniss.benyagoub@gmail.com>
2023-04-15 10:14:03 -0800

    c

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
-function toggleExpand(element, className) {
-  element.classList.toggle(className);
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-  // Send Coin
-  const sendArrow = document.querySelector('.swap_right .coin_selector_arrow');
-  const sendArrowMobile = document.querySelector('.swap_right .coin_selector_arrow_mobile');
   const sendExpand = document.querySelector('.send_coin_expand');
-  const sendExpandMobile = document.querySelector('.send_coin_expand_mobile');
-
-  // Receive Coin
-  const receiveArrow = document.querySelector('.swap_receive_right .coin_selector_arrow');
-  const receiveArrowMobile = document.querySelector('.swap_receive_right .coin_selector_arrow_mobile');
   const receiveExpand = document.querySelector('.receive_coin_expand');
-  const receiveExpandMobile = document.querySelector('.receive_coin_expand_mobile');
 
-  // Event listeners for desktop
-  sendArrow.addEventListener('click', () => toggleExpand(sendExpand, 'show'));
-  receiveArrow.addEventListener('click', () => toggleExpand(receiveExpand, 'show'));
+  function toggleExpand(el) {
+    el.classList.toggle('show');
+  }
 
-  // Event listeners for mobile
-  sendArrowMobile.addEventListener('click', () => toggleExpand(sendExpandMobile, 'show'));
-  receiveArrowMobile.addEventListener('click', () => toggleExpand(receiveExpandMobile, 'show'));
+  function handleClick(selector, target) {
+    document.querySelector(selector).addEventListener('click', function () {
+      toggleExpand(target);
+    });
+  }
+
+  handleClick('.swap_right .coin_selector_arrow', sendExpand);
+  handleClick('.swap_receive_right .coin_selector_arrow', receiveExpand);
+  handleClick('.swap_right .coin_selector_arrow_mobile', sendExpand);
+  handleClick('.swap_receive_right .coin_selector_arrow_mobile', receiveExpand);
 });
