6c6aea3d3d35e16524d8ce9b12e66911f0aaf2f5
Yanton92i <yaniss.benyagoub@gmail.com>
2023-08-03 22:27:54 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,32 @@
 window.addEventListener('load', async () => {
-  if (window.ethereum) {
-    window.web3 = new Web3(ethereum);
-    try {
-      await ethereum.enable();
-    } catch (error) {
-      console.error("User denied account access...")
-    }
-  }
-  else if (window.web3) {
-    window.web3 = new Web3(web3.currentProvider);
-  }
-  else {
-    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-  }
-  
-const connectButton = document.querySelector('.connect');
-const guessButton = document.querySelector('.send_button');
-
-// Make guess button unclickable at the start
-guessButton.style.pointerEvents = 'none';
-
-// Wait for user to click the connect button
-connectButton.addEventListener('click', async () => {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    const balanceWei = await web3.eth.getBalance(account);
-    const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
-
-    // Change the button text to show the wallet address
-    connectButton.textContent = account.slice(0, 4) + '...' + account.slice(-4);
-
-    if (balanceEth >= 0.0010) {
-      guessButton.style.pointerEvents = 'auto';
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+    } else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
     } else {
-      guessButton.style.pointerEvents = 'none';
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
-});
 
+    const connectButton = document.querySelector('.connect');
+    const guessButton = document.querySelector('.send_button');
 
+    // Make guess button unclickable at the start
+    guessButton.style.pointerEvents = 'none';
 
-  guessButton.style.pointerEvents = 'none';
+    // Wait for user to click the connect button
+    connectButton.addEventListener('click', async () => {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        const balanceWei = await web3.eth.getBalance(account);
+        const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+
+        // Change the button text to show the wallet address
+        connectButton.textContent = account.slice(0, 2) + '...' + account.slice(-3);
+
+        if (parseFloat(balanceEth) >= 0.0010) {
+            guessButton.style.pointerEvents = 'auto';
+        } else {
+            guessButton.style.pointerEvents = 'none';
+        }
+    });
 });
