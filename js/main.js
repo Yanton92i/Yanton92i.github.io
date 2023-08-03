40874c1cf4dbfad64806d4cfad137690efe5bc88
Yanton92i <yaniss.benyagoub@gmail.com>
2023-08-03 22:23:23 +0200

    js

--- js/main.js
+++ js/main.js
@@ -14,24 +14,29 @@ window.addEventListener('load', async () => {
     console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
   }
   
-  const connectButton = document.querySelector('.connect');
-  const guessButton = document.querySelector('.send_button');
+const connectButton = document.querySelector('.connect');
+const guessButton = document.querySelector('.send_button');
 
+// Make guess button unclickable at the start
+guessButton.style.pointerEvents = 'none';
+
+// Wait for user to click the connect button
 connectButton.addEventListener('click', async () => {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     const account = accounts[0];
     const balanceWei = await web3.eth.getBalance(account);
     const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
 
+    // Change the button text to show the wallet address
+    connectButton.textContent = account.slice(0, 4) + '...' + account.slice(-4);
+
     if (balanceEth >= 0.0010) {
       guessButton.style.pointerEvents = 'auto';
     } else {
       guessButton.style.pointerEvents = 'none';
     }
+});
 
-    // Change the button text to show the wallet address
-    connectButton.textContent = account.slice(0, 6) + '...' + account.slice(-4);
-  });
 
 
   guessButton.style.pointerEvents = 'none';
