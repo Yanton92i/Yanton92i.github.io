737684d7237b6991bbcf07dcaca992e5e360b6ad
Yanton92i <yaniss.benyagoub@gmail.com>
2023-08-11 03:40:54 +0200

    digits

--- js/connect.js
+++ js/connect.js
@@ -8,27 +8,26 @@ window.addEventListener('load', async () => {
   const connectButton = document.querySelector('.button_wallet');
   const spinButton = document.querySelector('.loot_button');
 
-  // Function to disable the spin button
   function disableSpinButton() {
     spinButton.style.pointerEvents = 'none';
     spinButton.style.cursor = 'not-allowed';
   }
 
-  // Function to enable the spin button
   function enableSpinButton() {
     spinButton.style.pointerEvents = 'auto';
     spinButton.style.cursor = 'pointer';
   }
 
-  // Disable the spin button at the start
   disableSpinButton();
 
-  // Wait for user to click the connect button
   connectButton.addEventListener('click', async () => {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     const account = accounts[0];
     const balanceWei = await web3.eth.getBalance(account);
     const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+
+    // Change the button text to show the wallet address
+    connectButton.textContent = account.slice(0, 4) + '...' + account.slice(-3);
 
     if (parseFloat(balanceEth) < 0.0010) {
       disableSpinButton();
