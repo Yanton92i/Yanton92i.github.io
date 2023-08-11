af4b27a427948b6d3b4ccfb8b7a61fb87589174c
Yanton92i <yaniss.benyagoub@gmail.com>
2023-08-11 04:24:12 +0200

    spin update

--- js/spin.js
+++ js/spin.js
@@ -1,8 +1,12 @@
 const spinButton = document.querySelector('.loot_button');
-let hasSpun = false; // You will need to replace this with a logic that checks if the user's wallet has already spun
+let whitelistSpots = 150; // Initial number of whitelist spots
+let hasSpun = false; // You will need to replace this with logic that checks if the user's wallet has already spun
 
 spinButton.addEventListener('click', function() {
   if (hasSpun) return;
+
+  // Adjust the probability for the whitelist spots
+  let whitelistProbability = (whitelistSpots / 150) * 49.7;
 
   let randomValue = Math.random() * 100;
   let result;
@@ -13,8 +17,10 @@ spinButton.addEventListener('click', function() {
     result = "popup_win_prize_2";
   } else if (randomValue < 0.8) {
     result = "popup_win_prize_3";
-  } else if (randomValue < 50.5) {
+  } else if (randomValue < whitelistProbability + 0.8) { // Adding the chances from the previous conditions
     result = "popup_win_wl";
+    whitelistSpots--; // Decrement the whitelist spots
+    document.querySelector('.spots').textContent = whitelistSpots; // Update the displayed number of spots
   } else {
     spinButton.textContent = "Nothing Found";
   }
