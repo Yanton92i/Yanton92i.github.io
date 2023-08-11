6b3bcde540b4edf70dba92a7375d03d18e1ba9d2
Yanton92i <yaniss.benyagoub@gmail.com>
2023-08-11 03:16:44 +0200

    changes

--- js/connect.js
+++ js/connect.js
@@ -1,9 +1,9 @@
 async function connectWallet() {
   if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
     try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const account = accounts[0];
-      checkBalance(account);
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      checkBalance();
     } catch (error) {
       console.error("User denied account access");
     }
@@ -12,12 +12,11 @@ async function connectWallet() {
   }
 }
 
-async function checkBalance(account) {
-  const web3 = new Web3(window.ethereum);
-  const balanceWei = await web3.eth.getBalance(account);
-  const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
-
-  const spinButton = document.querySelector('.loot_button');
+async function checkBalance() {
+  const accounts = await window.web3.eth.getAccounts();
+  const balanceWei = await window.web3.eth.getBalance(accounts[0]);
+  const balanceEth = window.web3.utils.fromWei(balanceWei, 'ether');
+  const spinButton = document.getElementById('spin-button');
 
   if (parseFloat(balanceEth) >= 0.001) {
     spinButton.classList.remove('disabled-button');
